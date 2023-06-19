ff616dc1e52a6d70038b51ee5bce8aeffbe9795b
Antoine Vacher <avacher@cyblex-consulting.com>
2023-06-19 15:22:58 +0200

    Added check 2.5.1

---
+++ checks/check-2_5_1.py
@@ -0,0 +1,46 @@
+from checker import Checker
+
+class Check_2_5_1(Checker):
+
+    def __init__(self, config, verbose=False):
+        
+        super().__init__(config, verbose)
+
+        self.id = "2.5.1"
+        self.title = "Ensure High Availability Configuration"
+        self.levels = [2]
+        self.auto = True
+        self.benchmark_version = "v1.1.0"
+
+    def do_check(self):
+        config_system_ha = self.get_config("system ha")
+
+        if config_system_ha is None:
+            self.set_message(f'No \"config system ha\" block defined')
+            return False
+
+        if "mode" not in config_system_ha.keys():
+            self.set_message(f'No HA mode configured')
+            return False
+
+        if not config_system_ha["mode"] == "a-p":
+            self.set_message(f'HA Mode is {config_system_ha["mode"]} and not \"a-p\"')
+            return False
+
+        if "group-name" not in config_system_ha.keys():
+            self.set_message(f'No HA group name configured')
+            return False
+        else:
+            self.add_message(f'HA Group is {config_system_ha["group-name"]}')
+
+        if "password" not in config_system_ha.keys():
+            self.set_message(f'No HA password configured')
+            return False
+
+        if "hbdev" not in config_system_ha.keys():
+            self.set_message(f'No HA Heartbeat configured')
+            return False
+        else:
+            self.add_message(f'HA hearbeat configuration : {config_system_ha["hbdev"]}')
+
+        return True
