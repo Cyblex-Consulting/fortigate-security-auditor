ff4cf9f6b68ce3e988e1084fa060c2145d92ab21
Antoine Vacher <avacher@cyblex-consulting.com>
2023-06-26 17:26:52 +0200

    Add check 3.1

---
+++ checks/check-3_1.py
@@ -0,0 +1,27 @@
+from checker import Checker
+
+class Check_3_1(Checker):
+
+    def __init__(self, config, verbose=False):
+        
+        super().__init__(config, verbose)
+
+        self.id = "3.1"
+        self.title = "Ensure that unused policies are reviewed regularly"
+        self.levels = [2]
+        self.auto = False
+        self.benchmark_version = "v1.1.0"
+        self.enabled = True
+
+    def do_check(self):
+
+        question = 'Unused policy shall be checked manually by resetting the counters and then checking which rule never matched.'
+        question += 'Was that performed and the result was satisfactory? (Y/n)'
+        
+        answer = self.ask(question)
+        if answer == 'n' or answer == 'N':
+            self.set_message("Manually set to not compliant")
+            return False
+        else:
+            self.set_message("Manually set to compliant")
+            return True
