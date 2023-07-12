058537a52f79b969eb265905785476fd67079a4b
Antoine Vacher <avacher@cyblex-consulting.com>
2023-07-12 17:28:46 +0200

    Add some custom checks by Cyblex

---
+++ checks/cyblex_1_0_0/check-2.py
@@ -0,0 +1,74 @@
+from checker import Checker
+import re
+
+class Check_Cyblex_2(Checker):
+
+    def __init__(self, firewall, display, verbose=False):
+        
+        super().__init__(firewall, display, verbose)
+
+        self.id = "2"
+        self.title = "Check proxy policies that looks temporary or debug"
+        self.levels = [1]
+        self.auto = False
+        self.benchmark_version = "1.0.0"
+        self.benchmark_author = "Cyblex"
+
+    def do_check(self):
+        config_proxy_policy = self.get_config("firewall proxy-policy")
+
+        if config_proxy_policy is None:
+            self.set_message(f'No \"config proxy-policy\" block defined')
+            return True
+        
+        REGEX = re.compile('TEMP|tmp|test|DBG|Debug|Open BAR', re.IGNORECASE)
+
+        suspicious_rules = []
+        if len(config_proxy_policy['edits']) > 0:
+            
+            for edit in config_proxy_policy['edits']:
+                suspicious = False
+
+                if "name" in edit.keys() and len(edit["name"]) > 0:
+                    name = edit["name"]
+                    if REGEX.search(name):
+                        suspicious = True
+                else:
+                    name = "<no name>"
+                
+                if "comments" in edit.keys():
+                    comments = edit['comments']
+
+                    if len(comments) > 0:
+                        # Dirty semi workaround for https://github.com/ssato/python-anyconfig-fortios-backend/issues/4
+                        if isinstance(comments, list):
+                            comments = " ".join(comments)
+
+                        if REGEX.search(comments):
+                            suspicious = True
+                else:
+                    comments = "<no comment>"
+
+                if suspicious:
+                    suspicious_rules.append({"uuid": edit["uuid"], "name": name, "comments": comments})
+                        
+            if len(suspicious_rules) > 0:
+                self.add_question_context("The following policies have suspicious name or suspicious comment:")
+                for rule in suspicious_rules:
+                   self.add_question_context(f"{rule['uuid']}: {rule['name']}")
+                   self.add_question_context(f"{rule['comments']}")
+                answer = self.ask('Is that Ok? (Y/n)')
+                if answer == 'n' or answer == 'N':
+                    self.set_message("Manually set to not compliant")
+                    return False
+                else:
+                    self.set_message("Manually set to compliant")
+                    return True
+            else:
+                self.set_message("No policy with suspicious name found")
+                return True
+        else:
+            self.add_message('There is no policy defined')
+            return True
+
+        return None
